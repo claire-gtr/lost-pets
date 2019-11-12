867ee30b8c32b79252c34d3c6f487613332169a2
claire gautier <claire.gautier@edhec.com>
2019-11-12 18:59:03 +0100

    fixed mistake in create route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   #get '/pets', to: 'pets#index'
   #get '/pets/:id', to: 'pets#show', as: :pet
   #get '/pets/new', to: 'pets#new', as: :new_pet
-  #post '/pets/:id/new', to: 'pets#create'
+  #post '/pets', to: 'pets#create'
   # get '/pets/:id/edit', to: 'pets#edit'
   # patch '/pets/:id', to: 'pets#update'
   #delete '/pets/:id', to: 'pets#destroy'
